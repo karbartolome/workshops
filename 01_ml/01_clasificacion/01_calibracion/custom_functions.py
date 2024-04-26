29745274208be66df1c5cdb8a53fc6262938e8e9
Karina Bartolomé <karinabartolome@hotmail.com>
2024-04-25 23:45:21 -0300

    :recycle: reorder

--- 01_ml/01_clasificacion/01_calibracion/custom_functions.py
+++ 01_ml/01_clasificacion/01_calibracion/custom_functions.py
@@ -2,19 +2,38 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.calibration import calibration_curve
+from sklearn.calibration import calibration_curve, CalibratedClassifierCV
 from IPython.display import display
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    roc_auc_score,
+    log_loss,
+    recall_score,
+    brier_score_loss,
+)
+from great_tables import GT, from_column, style, loc, html
+
+
+color_verde = "#255255"
+color_verde_claro = "#BDCBCC"
 
 
 def plot_distribution(
-    data, obs_column="y_obs", pred_column="pred_prob", positive_class=1,
-    class_0_color=None, class_1_color=None
+    data,
+    obs_column="y_obs",
+    pred_column="pred_prob",
+    positive_class=1,
+    class_0_color=None,
+    class_1_color=None,
 ):
     if class_0_color is None:
-        class_0_color = 'blue'
+        class_0_color = "blue"
     if class_1_color is None:
-        class_1_color = 'red'
-    #sns.set(font_scale=0.8)
+        class_1_color = "red"
+    # sns.set(font_scale=0.8)
     plt.figure(figsize=(4, 2))
     sns.kdeplot(
         data=data[data[obs_column] == positive_class],
@@ -34,7 +53,15 @@ def plot_distribution(
 
 
 def plot_calibration_models(
-    y_obs, y_pred_prob, y_pred_prob_cal, bins=10, strategy="uniform", figsize=(6,6),display_bins=False
+    y_obs,
+    y_pred_prob,
+    y_pred_prob_cal_sigmoid,
+    y_pred_prob_cal_isotonic,
+    bins=10,
+    strategy="uniform",
+    figsize=(6, 6),
+    display_bins=False,
+    model_name="",
 ) -> None:
     """
     Gráfico comparando probabilidad vs probabilidad calibrada
@@ -59,9 +86,9 @@ def plot_calibration_models(
         color="red",
         label="Probabilidades no calibradas",
     )
-    if y_pred_prob_cal is not None:
+    if y_pred_prob_cal_sigmoid.notna().any():
         prob_true_cal, prob_pred_cal = calibration_curve(
-            y_obs, y_pred_prob_cal, n_bins=bins, strategy=strategy
+            y_obs, y_pred_prob_cal_sigmoid, n_bins=bins, strategy=strategy
         )
 
         plt.plot(
@@ -69,27 +96,120 @@ def plot_calibration_models(
             prob_true_cal,
             marker="o",
             color="blue",
-            label="Probabilidades calibradas",
+            label="Probabilidades calibradas (sigmoide)",
+        )
+    if y_pred_prob_cal_isotonic.notna().any():
+        prob_true_cal, prob_pred_cal = calibration_curve(
+            y_obs, y_pred_prob_cal_isotonic, n_bins=bins, strategy=strategy
         )
 
-    plt.axline([0, 0], [1, 1], label="Calibración perfecta", c="green")
-    plt.text(0.8, 0.85, "Línea 45°", fontsize=10, rotation=45, c='green')
+        plt.plot(
+            prob_pred_cal,
+            prob_true_cal,
+            marker="o",
+            color='green',
+            label="Probabilidades calibradas (isotonic)",
+        )
+
+    plt.axline([0, 0], [1, 1], label="Calibración perfecta", c=color_verde)
+    plt.text(0.8, 0.85, "Línea 45°", fontsize=10, rotation=45, c=color_verde)
 
     plt.legend()
-    #plt.title("Probabilidades calibradas vs no calibradas")
+    plt.title(f"Curvas de calibración {model_name}")
     plt.xlabel("Probabilidad predicha promedio")
     plt.ylabel("Fracción de positivos (clase positiva=1)")
     plt.text(0.10, 0.70, "Subestimación", fontsize=10, rotation=45)
     plt.text(0.70, 0.10, "Sobreestimación", fontsize=10, rotation=45)
 
-    if display_bins:
-        return (pd.DataFrame({
-            'prob_true': prob_true,
-            'prob_pred': prob_pred
-        }))
+
+def calibrate_model(
+    pipe,
+    X_valid,
+    y_valid,
+    X_test,
+    y_test,
+    cal_sigmoid=True,
+    cal_isotonic=True,
+    model_name="",
+    plot_cal=True,
+):
+    preds = pd.DataFrame(
+        {
+            "y_obs": y_test,
+            "pred_class": pipe.predict(X_test),
+            "pred_prob": pipe.predict_proba(X_test)[:, 1],
+        }
+    )
+
+    preds["pred_sigmoid"] = np.nan
+    preds["pred_isotonic"] = np.nan
+    pipe_sigmoid = None
+    pipe_isotonic = None
+
+    if cal_sigmoid:
+        # Calibración sigmoide (Platt's scaling)
+        pipe_sigmoid = CalibratedClassifierCV(pipe, cv="prefit", method="sigmoid")
+        pipe_sigmoid.fit(X_valid, y_valid)
+        prob_pos_sigmoid = pipe_sigmoid.predict_proba(X_test)[:, 1]
+        preds["pred_sigmoid"] = prob_pos_sigmoid
+        preds["pred_class_sigmoid"] = [1 if i > 0.5 else 0 for i in prob_pos_sigmoid]
+
+    if cal_isotonic:
+        # Calibración isotónica
+        pipe_isotonic = CalibratedClassifierCV(pipe, cv="prefit", method="isotonic")
+        pipe_isotonic.fit(X_valid, y_valid)
+        prob_pos_isotonic = pipe_isotonic.predict_proba(X_test)[:, 1]
+        preds["pred_isotonic"] = prob_pos_isotonic
+        preds["pred_class_isotonic"] = [1 if i > 0.5 else 0 for i in prob_pos_isotonic]
+
+    if plot_cal:
+        plot_calibration_models(
+            y_obs=preds["y_obs"],
+            y_pred_prob=preds["pred_prob"],
+            y_pred_prob_cal_sigmoid=preds["pred_sigmoid"],
+            y_pred_prob_cal_isotonic=preds["pred_isotonic"],
+            bins=7,
+            strategy="uniform",
+            figsize=(4, 4),
+            display_bins=True,
+            model_name=model_name,
+        )
+        plt.legend(loc="center left", bbox_to_anchor=(0, -0.3))
+        plt.show()
+
+    return pipe_sigmoid, pipe_isotonic, preds
 
 
-def gain_table(preds, pred_column="pred_prob", bins=10, title=""):
+def plot_calibration(
+    preds, y_pred_prob_calibrated, y_pred_prob="pred_prob", model_name=""
+):
+    plt.figure(figsize=(4, 4))
+    sns.scatterplot(
+        x=y_pred_prob,
+        y=y_pred_prob_calibrated,
+        data=preds,
+        color="blue",
+        alpha=0.7,
+        label="Calibración sklearn",
+    )
+    sns.scatterplot(
+        x="pred_prob",
+        y="y_obs",
+        data=preds,
+        alpha=0.3,
+        color="black",
+        label="Valores observados",
+    )
+    plt.axline([0, 0], [1, 1], label="Calibración perfecta", c=color_verde)
+    plt.text(0.8, 0.85, "Línea 45°", fontsize=10, rotation=45, c=color_verde)
+    plt.ylabel("Probabilidad calibrada")
+    plt.xlabel("Probabilidad no calibrada")
+    plt.legend(loc="center left", bbox_to_anchor=(0, -0.3))
+    plt.title(f"Calibración {model_name}")
+    plt.ylim([-0.01, 1.01])
+
+
+def calibration_table(preds, pred_column="pred_prob", bins=10, title=""):
     preds["Bin"] = pd.qcut(preds[pred_column], q=bins, duplicates="drop")
 
     gain_table = (
@@ -104,24 +224,38 @@ def gain_table(preds, pred_column="pred_prob", bins=10, title=""):
             diff=lambda x: np.abs(x["avg_prob"] - x["perc_clase1"]),
         )
     )
-    gain_table["N_acum"] = gain_table["N"].cumsum()
-    gain_table["N_clase1_acum"] = gain_table["N_clase1"].cumsum()
-    gain_table["TPR"] = gain_table["N_clase1_acum"] / gain_table["N_acum"]
+
     gain_table["Bin"] = gain_table["Bin"].apply(
         lambda x: f"({x.left*100:.2f}%, {x.right*100:.2f}%]"
     )
+
     gain_table = (
-        gain_table.style.background_gradient(axis=0)
-        .format(
-            {
-                "avg_prob": "{:.2%}",
-                "perc_clase1": "{:.2%}",
-                "diff": "{:.3f}",
-                "TPR": "{:.2%}",
-            }
+        GT(gain_table.round(2))
+        .cols_label(
+            avg_prob=html("Probabilidad<br>promedio"),
+            perc_clase1=html("% Clase 1<br>(observado)"),
+            diff=html("Diferencia <br>|avg prob - % clase 1|"),
+            N_clase1=html("N Clase 1"),
         )
-        .background_gradient(subset=["diff"], cmap="Reds", vmin=0, vmax=1)
-        .hide(axis=0)
-        #.set_caption(f"Tabla de ganancias {title}")
+        .fmt_percent(["avg_prob", "perc_clase1", 'diff'])
+        .data_color(
+            columns=["avg_prob", "perc_clase1"],
+            palette=[color_verde_claro, color_verde],
+        )
+        .data_color(columns=["diff"], palette=["white", "red"], domain=[0,1])
+        #.tab_header(title)
     )
     return display(gain_table)
+
+
+def calculate_clf_metrics(y_true, y_pred, y_pred_prob, name):
+    metrics = {}
+    metrics["Accuracy"] = accuracy_score(y_true, y_pred)
+    metrics["Precision"] = precision_score(y_true, y_pred)
+    metrics["Recall"] = recall_score(y_true, y_pred)
+    metrics["F1"] = f1_score(y_true, y_pred)
+    metrics["ROC AUC"] = roc_auc_score(y_true, y_pred_prob)
+    metrics["Log Loss"] = log_loss(y_true, y_pred_prob)
+    metrics["Brier Loss"] = brier_score_loss(y_true, y_pred_prob)
+    metrics_df = pd.DataFrame(metrics, index=[name])
+    return metrics_df
